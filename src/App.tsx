ecd7665bd1b023a7aca7b7ce14fdb15afc06cc70
Henry Lin <henry.ys.lin@gmail.com>
2024-03-17 10:15:36 +0800

    Minor visual fixes

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import SolveMode from './ui/modes/SolveMode';
 import { Mode } from './data/primitives';
 import CreateMode from './ui/modes/CreateMode';
 import ModeButton from './ui/modes/ModeButton';
+import { FiChevronDown } from 'react-icons/fi';
 
 const SUPPORTED_THEMES = [
   'light',
@@ -59,7 +60,7 @@ export default function App() {
           <Analytics />
           <SpeedInsights />
           <div className="flex flex-col items-stretch min-h-full w-full">
-            <header className="flex justify-between items-center gap-4 px-8 py-2">
+            <header className="flex flex-wrap justify-between items-center gap-4 px-8 py-2">
               <h1 className="text-3xl text-neutral-content">Logic Pad</h1>
               <div role="tablist" className="tabs tabs-boxed tabs-lg">
                 <ModeButton
@@ -80,15 +81,7 @@ export default function App() {
               <div className="dropdown dropdown-end">
                 <div tabIndex={0} role="button" className="btn m-1">
                   Theme
-                  <svg
-                    width="12px"
-                    height="12px"
-                    className="h-2 w-2 fill-current opacity-60 inline-block"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 2048 2048"
-                  >
-                    <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-                  </svg>
+                  <FiChevronDown />
                 </div>
                 <ul
                   tabIndex={0}

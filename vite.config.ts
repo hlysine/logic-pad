425971a8bc0a9b2f7ae944bd5fba9598aef16b5b
Henry Lin <henry.ys.lin@gmail.com>
2024-03-17 19:47:14 +0800

    Add million

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import million from 'million/compiler';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
 });

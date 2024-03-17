0bfd881a5d265cc11e8309a47f2a3f6ef44a920c
Henry Lin <henry.ys.lin@gmail.com>
2024-03-17 20:33:09 +0800

    Refactor roadmap

--- src/ui/modes/Roadmap.tsx
+++ src/ui/modes/Roadmap.tsx
@@ -1,122 +1,81 @@
 import { memo } from 'react';
+import { cn } from '../../utils';
+
+type RoadmapItem = string | [string, ...RoadmapItem[]];
+
+const roadmap: RoadmapItem[] = [
+  '~Implement puzzle grid',
+  '~Implement click and drag mouse input',
+  '~Implement merged tiles',
+  '~Implement rules UI',
+  '~Implement color themes',
+  [
+    'Add rules and symbols',
+    '~Area number',
+    '~Letter',
+    "~Don't make this pattern",
+    '~Complete the pattern',
+    '~Connect all ___ cells',
+    '~Underclued grid',
+    '~Viewpoint number',
+    'Dart number',
+    'Lotus',
+    'Spiral',
+    'All ___ regions have area #',
+    'Exactly # symbols per ___ region',
+  ],
+  '~Add win confirmation',
+  '~Add undo and restart',
+  'Add flood painting',
+  'Add tile counting by holding Ctrl',
+  'Implement puzzle serialization',
+  [
+    'Optimizations',
+    'Optimize merged cells',
+    '~Add transitions to laggy buttons',
+    '~Memoize components',
+  ],
+  [
+    'Puzzle editor',
+    'Add color, fix and merge tools',
+    'Add a tool for each symbol type',
+    'Hide tools behind search bar',
+    'Add configurations for each rule',
+    'Hide rules behind search bar',
+    'Add puzzle name and author fields',
+    'Add difficulty field',
+  ],
+];
+
+function renderString(item: string) {
+  return (
+    <li className={cn(item.startsWith('~') && 'opacity-50')}>
+      <a>{item.startsWith('~') ? item.substring(1) : item}</a>
+    </li>
+  );
+}
+
+function renderItem(item: RoadmapItem) {
+  if (typeof item === 'string') {
+    return renderString(item);
+  }
+  const [title, ...subItems] = item;
+  return (
+    <li>
+      <details open>
+        <summary className={cn(title.startsWith('~') && 'opacity-50')}>
+          {title.startsWith('~') ? title.substring(1) : title}
+        </summary>
+        <ul>{subItems.map(renderItem)}</ul>
+      </details>
+    </li>
+  );
+}
 
 export default memo(function Roadmap() {
   return (
     <ul className="menu menu-vertical flex-nowrap bg-base-200 rounded-box overflow-y-auto overflow-x-visible basis-0 flex-1 justify-self-stretch">
-      <li className="opacity-50">
-        <a>Implement puzzle grid</a>
-      </li>
-      <li className="opacity-50">
-        <a>Implement click and drag mouse input</a>
-      </li>
-      <li className="opacity-50">
-        <a>Implement merged tiles</a>
-      </li>
-      <li className="opacity-50">
-        <a>Implement rules UI</a>
-      </li>
-      <li className="opacity-50">
-        <a>Implement color themes</a>
-      </li>
-      <li>
-        <details open>
-          <summary>Add rules and symbols</summary>
-          <ul>
-            <li className="opacity-50">
-              <a>Area number</a>
-            </li>
-            <li className="opacity-50">
-              <a>Letter</a>
-            </li>
-            <li className="opacity-50">
-              <a>Don&#39;t make this pattern</a>
-            </li>
-            <li className="opacity-50">
-              <a>Complete the pattern</a>
-            </li>
-            <li className="opacity-50">
-              <a>Connect all ___ cells</a>
-            </li>
-            <li className="opacity-50">
-              <a>Underclued grid</a>
-            </li>
-            <li className="opacity-50">
-              <a>Viewpoint number</a>
-            </li>
-            <li>
-              <a>Dart number</a>
-            </li>
-            <li>
-              <a>Lotus</a>
-            </li>
-            <li>
-              <a>Spiral</a>
-            </li>
-            <li>
-              <a>All ___ regions have area #</a>
-            </li>
-            <li>
-              <a>Exactly # symbols per ___ region</a>
-            </li>
-          </ul>
-        </details>
-      </li>
-      <li className="opacity-50">
-        <a>Implement win confirmation</a>
-      </li>
-      <li className="opacity-50">
-        <a>Add undo and restart</a>
-      </li>
-      <li>
-        <a>Add flood painting</a>
-      </li>
-      <li>
-        <a>Add tile counting by holding Ctrl</a>
-      </li>
-      <li>
-        <a>Implement puzzle serialization</a>
-      </li>
-      <li>
-        <details open>
-          <summary>Optimizations</summary>
-          <ul>
-            <li>
-              <a>Optimize merged cells</a>
-            </li>
-            <li className="opacity-50">
-              <a>Add transitions to laggy buttons</a>
-            </li>
-            <li className="opacity-50">
-              <a>Memoize components</a>
-            </li>
-          </ul>
-        </details>
-      </li>
-      <li>
-        <details open>
-          <summary>Puzzle editor</summary>
-          <ul>
-            <li>
-              <a>Add color, fix and merge tools</a>
-            </li>
-            <li>
-              <a>Add a tool for each symbol type</a>
-            </li>
-            <li>
-              <a>Hide tools behind search bar</a>
-            </li>
-            <li>
-              <a>Add configurations for each rule</a>
-            </li>
-            <li>
-              <a>Hide rules behind search bar</a>
-            </li>
-            <li>
-              <a>Add puzzle name and author fields</a>
-            </li>
-          </ul>
-        </details>
-      </li>
+      {roadmap.map(renderItem)}
     </ul>
   );
 });

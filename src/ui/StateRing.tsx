2d39be6dc10d787106fa1341ff56cef0317bd4be
Henry Lin <henry.ys.lin@gmail.com>
2024-04-06 16:38:37 +0800

    Improve glow implementation

--- src/ui/StateRing.tsx
+++ src/ui/StateRing.tsx
@@ -15,9 +15,12 @@ export default memo(function StateRing({ children }: GridRingProps) {
       className={cn(
         'w-fit h-fit border-4 p-4 rounded-xl transition-shadow',
         ringBorder(state.final),
-        state.final === State.Satisfied && 'bg-smart-success'
+        state.final === State.Satisfied
+          ? 'first:*:opacity-100'
+          : 'first:*:opacity-0'
       )}
     >
+      <div className="block fixed inset-0 transition-all duration-[3s] ease-in bg-radient-circle-c from-transparent to-success/15 z-[1000] pointer-events-none"></div>
       {children}
     </div>
   );

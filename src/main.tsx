a1dcfe78219e2b37edeba7f8d0811193651d71e5
Henry Lin <henry.ys.lin@gmail.com>
2024-04-08 14:29:48 +0800

    Add PWA support and fix code splitting

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,9 @@ import { SpeedInsights } from '@vercel/speed-insights/react';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './ui/router/routeTree.gen';
 import NotFound from './ui/router/NotFound';
-import { themeKey } from './ui/ThemeSwitcher';
 
 // load the selected theme early to avoid flicker
-const savedTheme = localStorage.getItem(themeKey) ?? 'dracula';
+const savedTheme = localStorage.getItem('theme') ?? 'dracula';
 document.documentElement.dataset.theme = savedTheme;
 
 const router = createRouter({
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import million from 'million/compiler';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,12 @@ export default defineConfig({
     react(),
     TanStackRouterVite({
       generatedRouteTree: 'src/ui/router/routeTree.gen.ts',
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        theme_color: '#414558',
+      },
     }),
   ],
 });

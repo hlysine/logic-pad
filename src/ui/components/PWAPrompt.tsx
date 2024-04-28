1819a61b01410e50c74104c13d20e56216fbb342
Henry Lin <henry.ys.lin@gmail.com>
2024-04-28 16:07:58 +0800

    Reduce PWA prompt width

--- src/ui/components/PWAPrompt.tsx
+++ src/ui/components/PWAPrompt.tsx
@@ -9,7 +9,7 @@ export default memo(function PWAPrompt() {
   return (
     <div
       role="alert"
-      className="alert fixed top-2 right-2 max-w-[calc(100%-1rem)] shadow-xl z-50"
+      className="alert min-w-[min(100%-1rem,512px)] w-fit fixed top-2 right-2 max-w-[calc(100%-1rem)] shadow-xl z-50"
     >
       <FiAlertCircle />
       <span>Update available. Reload site now?</span>

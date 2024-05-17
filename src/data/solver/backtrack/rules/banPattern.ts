d4b339e196bd86b44c1df6b18aae6bfcbcb47733
ALaggyDev <renlin456123@gmail.com>
2024-05-17 23:32:25 +0800

    Add ban pattern optimization

--- src/data/solver/backtrack/rules/banPattern.ts
+++ src/data/solver/backtrack/rules/banPattern.ts
@@ -3,7 +3,9 @@ import BanPatternRule from '../../../rules/banPatternRule';
 import BTModule, {
   BTColor,
   BTGridData,
+  BTTile,
   CheckResult,
+  Rating,
   colorToBTTile,
 } from '../data';
 
@@ -24,6 +26,8 @@ export default class BanPatternBTModule extends BTModule {
     grid: BTGridData,
     positions: Position[]
   ): CheckResult | false {
+    const ratings: Rating[] = [];
+
     // TODO: Do not iterate positions
     for (const { x, y } of positions) {
       const tile = grid.getTile(x, y) as BTColor;
@@ -39,20 +43,41 @@ export default class BanPatternBTModule extends BTModule {
           )
             continue;
 
-          if (
-            shape.elements.every(
-              element =>
-                grid.getTile(
-                  element.x - origin.x + x,
-                  element.y - origin.y + y
-                ) === colorToBTTile(element.color)
-            )
-          )
-            return false;
+          // We add a tile into ratings if that tile is the only mismatched tile
+          let match = true;
+          let mismatchPos: Position | null = null;
+          for (const element of shape.elements) {
+            const eleTile = grid.getTile(
+              element.x - origin.x + x,
+              element.y - origin.y + y
+            );
+            if (eleTile === BTTile.Empty) {
+              if (match) {
+                match = false;
+
+                mismatchPos = {
+                  x: element.x - origin.x + x,
+                  y: element.y - origin.y + y,
+                };
+              } else {
+                mismatchPos = null;
+                break;
+              }
+            } else if (eleTile !== colorToBTTile(element.color)) {
+              match = false;
+              mismatchPos = null;
+              break;
+            }
+          }
+
+          if (match) return false;
+          if (mismatchPos) ratings.push({ pos: mismatchPos, score: 10000 });
         }
       }
     }
 
-    return { tilesNeedCheck: null, ratings: null };
+    // TODO: Redesign API - This is not the best!
+    // Ratings refresh on every backtrack! Ratings should be appended on the previous result instead of over-writing!
+    return { tilesNeedCheck: null, ratings };
   }
 }

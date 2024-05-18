8ce32226099bd769c9fed218892c92bce221eccd
Henry Lin <henry.ys.lin@gmail.com>
2024-05-18 23:33:02 +0800

    Remove roadmap

--- src/routes/_layout.lazy.tsx
+++ src/routes/_layout.lazy.tsx
@@ -1,4 +1,3 @@
-import Roadmap from '../ui/components/Roadmap';
 import ModeSwitcher from '../ui/components/ModeSwitcher';
 import { Link, Outlet, createLazyFileRoute } from '@tanstack/react-router';
 import { memo } from 'react';
@@ -9,7 +8,7 @@ export const Route = createLazyFileRoute('/_layout')({
     return (
       <>
         <header className="flex flex-wrap justify-between items-center gap-4 px-8 py-2 shadow-md">
-          <div className="flex flex-wrap grow shrink items-center gap-4">
+          <div className="flex xl:basis-[320px] flex-wrap grow shrink items-center gap-4">
             <Link
               to="/"
               className="text-3xl text-neutral-content flex items-center gap-2"
@@ -17,19 +16,6 @@ export const Route = createLazyFileRoute('/_layout')({
               <img src="/logo.svg" className="w-8 h-8 inline-block" />
               Logic Pad
             </Link>
-            <ul className="menu menu-horizontal bg-base-200 text-base-content rounded-box">
-              <li className="dropdown dropdown-bottom">
-                <button tabIndex={0} role="button">
-                  Roadmap
-                </button>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content menu menu-vertical min-w-[300px] max-h-[calc(100vh-100px)] flex-nowrap bg-base-200 rounded-box overflow-y-auto overflow-x-visible text-base-content z-50"
-                >
-                  <Roadmap />
-                </ul>
-              </li>
-            </ul>
           </div>
           <ModeSwitcher />
           <QuickAccessBar className="xl:basis-[320px] grow shrink justify-end" />

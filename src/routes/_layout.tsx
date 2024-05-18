936b2561ace4279a5ad8a6234affcd7eded09981
Henry Lin <henry.ys.lin@gmail.com>
2024-05-18 09:46:54 +0800

    Add link to front page

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -1,6 +1,6 @@
 import Roadmap from '../ui/components/Roadmap';
 import ModeSwitcher from '../ui/components/ModeSwitcher';
-import { Outlet, createFileRoute } from '@tanstack/react-router';
+import { Link, Outlet, createFileRoute } from '@tanstack/react-router';
 import { memo } from 'react';
 import QuickAccessBar from '../ui/components/QuickAccessBar';
 
@@ -10,10 +10,13 @@ export const Route = createFileRoute('/_layout')({
       <>
         <header className="flex flex-wrap justify-between items-center gap-4 px-8 py-2 shadow-md">
           <div className="flex flex-wrap grow shrink items-center gap-4">
-            <h1 className="text-3xl text-neutral-content flex items-center gap-2">
+            <Link
+              to="/"
+              className="text-3xl text-neutral-content flex items-center gap-2"
+            >
               <img src="/logo.svg" className="w-8 h-8 inline-block" />
               Logic Pad
-            </h1>
+            </Link>
             <ul className="menu menu-horizontal bg-base-200 text-base-content rounded-box">
               <li className="dropdown dropdown-bottom">
                 <button tabIndex={0} role="button">

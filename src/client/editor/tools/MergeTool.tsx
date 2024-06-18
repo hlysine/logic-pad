c3c5ee364f60653c3741a00a4a95cd105ee94a4a
Henry Lin <henry.ys.lin@gmail.com>
2024-06-18 17:30:17 +0800

    Rework control scheme of merge tool to support mobile

--- src/client/editor/tools/MergeTool.tsx
+++ src/client/editor/tools/MergeTool.tsx
@@ -12,48 +12,57 @@ function MergeToolOverlay() {
     <PointerCaptureOverlay
       width={grid.width}
       height={grid.height}
+      step={0.01}
       colorMap={() => false}
       onTileClick={(x, y, from, to) => {
         if (x < 0 || y < 0 || x >= grid.width || y >= grid.height) {
           location.current = null;
           return;
         }
-        if (location.current === null) {
-          location.current = { x, y };
+        const tx = Math.floor(x);
+        const ty = Math.floor(y);
+        if (
+          location.current != null &&
+          location.current.x === tx &&
+          location.current.y === ty
+        ) {
           return;
         }
-        if (
-          Math.abs(location.current.x - x) +
-            Math.abs(location.current.y - y) ===
-          1
-        ) {
-          if (from === Color.Dark || to === Color.Dark) {
-            setGrid(
-              grid.withConnections(con =>
-                con.addEdge({
-                  x1: x,
-                  y1: y,
-                  x2: location.current!.x,
-                  y2: location.current!.y,
-                })
-              )
-            );
-          } else if (from === Color.Light || to === Color.Light) {
-            setGrid(
-              grid.withConnections(con =>
-                con.removeEdge({
-                  x1: x,
-                  y1: y,
-                  x2: location.current!.x,
-                  y2: location.current!.y,
-                })
-              )
-            );
-          }
+        let cx, cy;
+        if (Math.abs(tx + 0.5 - x) < 0.25 && Math.abs(ty + 0.5 - y) < 0.25)
+          return;
+        if (Math.abs(tx + 0.5 - x) > Math.abs(ty + 0.5 - y)) {
+          cx = x > tx + 0.5 ? tx + 1 : tx - 1;
+          cy = ty;
+        } else {
+          cx = tx;
+          cy = y > ty + 0.5 ? ty + 1 : ty - 1;
         }
-        location.current = { x, y };
+        if (from === Color.Dark || to === Color.Dark) {
+          setGrid(
+            grid.withConnections(con =>
+              con.addEdge({
+                x1: tx,
+                y1: ty,
+                x2: cx,
+                y2: cy,
+              })
+            )
+          );
+        } else if (from === Color.Light || to === Color.Light) {
+          setGrid(
+            grid.withConnections(con =>
+              con.removeEdge({
+                x1: tx,
+                y1: ty,
+                x2: cx,
+                y2: cy,
+              })
+            )
+          );
+        }
+        location.current = { x: tx, y: ty };
       }}
-      onPointerUp={() => (location.current = null)}
     />
   );
 }
@@ -64,7 +73,7 @@ export default memo(function MergeTool() {
       id="merge"
       order={3}
       name="Merge tiles"
-      description="Left click and drag to merge tiles. Right click and drag to split tiles."
+      description="Left click a gap to merge tiles. Right click to split tiles."
       hotkey="d"
       gridOverlay={<MergeToolOverlay />}
       onTileClick={null}

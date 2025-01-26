1d1536e9670c88d40acdbe1129f07fa82a633c20
Henry Lin <henry.ys.lin@gmail.com>
2025-01-26 22:25:54 +0800

    Update test 4

--- src/client/components/PWAPrompt.tsx
+++ src/client/components/PWAPrompt.tsx
@@ -16,7 +16,7 @@ export default memo(function PWAPrompt() {
       className="alert min-w-[min(100%-1rem,512px)] w-fit fixed top-2 right-2 max-w-[calc(100%-1rem)] shadow-xl z-50"
     >
       <FiAlertCircle />
-      <span>Update available. Reload site now? 3</span>
+      <span>Update available. Reload site now?</span>
       <div>
         <button
           type="button"

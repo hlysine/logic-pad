768380c0ab3934955278ee922785b7978c69a5bf
Henry Lin <henry.ys.lin@gmail.com>
2025-03-20 00:00:36 +0800

    Move insight solver to below universal
    This is to prevent auto from picking it up since it's still rudimentary right now

--- packages/logic-core/src/data/solver/allSolvers.ts
+++ packages/logic-core/src/data/solver/allSolvers.ts
@@ -4,7 +4,7 @@ import BacktrackSolver from './backtrack/backtrackSolver.js';
 import Z3Solver from './z3/z3Solver.js';
 import CspuzSolver from './cspuz/cspuzSolver.js';
 import AutoSolver from './auto/autoSolver.js';
-import InsightDrivenSolver from "./insightDriven/insightDrivenSolver.js";
+import InsightDrivenSolver from './insightDriven/insightDrivenSolver.js';
 
 const allSolvers = new Map<string, Solver>();
 
@@ -12,11 +12,14 @@ function register(prototype: Solver) {
   allSolvers.set(prototype.id, prototype);
 }
 
-register(new InsightDrivenSolver());
 register(new AutoSolver());
 register(new CspuzSolver());
 register(new BacktrackSolver());
 register(new UniversalSolver());
+
+// Solvers below the unviersal solver will not be picked by the auto solver
+
+register(new InsightDrivenSolver());
 register(new Z3Solver());
 
 export { allSolvers };

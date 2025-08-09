d2f832dc61aa10cd5a5ff6037f7d8c718cb23322
Henry Lin <henry.ys.lin@gmail.com>
2025-08-09 13:19:06 +0800

    Toast when puzzle not found

--- src/client/routes/_context._layout.edit.$puzzleId.tsx
+++ src/client/routes/_context._layout.edit.$puzzleId.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 import { queryOptions } from '@tanstack/react-query';
 import { api, queryClient } from '../online/api';
+import toast from 'react-hot-toast';
 
 export const puzzleQueryOptions = (puzzleId: string) =>
   queryOptions({
@@ -16,6 +17,7 @@ export const Route = createFileRoute('/_context/_layout/edit/$puzzleId')({
         puzzleQueryOptions(params.puzzleId)
       );
     } catch (error) {
+      toast.error((error as Error).message);
       throw redirect({
         to: '/create',
       });
